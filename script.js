8101a1c575ba4c8c9dc99da16ad57dae275b7d79
Mostafa Nassar <81562906+mostafahosam254@users.noreply.github.com>
2025-03-20 11:28:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,143 @@
+//هنا هيطبعلنا اشعار ب الجملة الي بين علامات التنصيص
+//طبعا الاشعار دا بيكون جي من المتصفح مش من الموقع نفسه
+
+// alert("hello world");
+
+
+//console
+//try action and clear()
+
+//دي هتطبعلنا النص ف الكونسول الخاص بالمتصفح
+// console.log("ethical hacking way ");
+// console.log("hello world");
+
+// // هتطبع النص في هيئة خطأ
+// console.error("error 404");
+// console.error("mostafa");
+
+//لتعريف متغير بنستخدم 
+//var,let,const
+/* 
+var
+
+ يتم استخدامها لإعلان متغيرات ذات نطاق دالة أو نطاق واسع  و مفتوح للاستدعاء.
+
+let
+
+ يتم استخدامها لإعلان متغيرات ذات نطاق اقل و يتم استدعائها بفنكشن اقل.
+
+const
+
+  يتم استخدامها لإعلان متغيرات ثابتة لا يمكن تغييرها.
+*/
+
+// var a =10;
+// let b =20;
+// const c =30;
+
+//we cannot
+  // c=90;
+
+
+// console.log(`value of a is ${a}`);
+// console.log(`afahfjkashfhas ${b}`);
+// console.log(`afahfjkashfhas ${c}`);
+
+// console.log(`data type of b is `+ typeof(b));
+
+// console.log(b);
+
+// var g = b>a;
+
+// console.log(g);
+
+
+
+// i++ = i+1
+
+// for (let i = 0; i <= 20; i++) {
+
+//     console.log(i);
+  
+// }
+
+
+// var x = 0 ;
+// while (x<=20) {
+//   console.log(x);
+//   x++;
+// }
+
+
+
+// const x = 19;
+// const x =  29;
+// const x = 20;
+
+// if (x==20) {
+//     x=89;
+//     console.log(`x value is ${x}` );
+//   }
+
+// else if (x>20) {
+//     console.log("x value is >20")
+// }
+
+// else{
+//     console.log("x value is < 20")
+// }
+
+
+
+
+
+
+//Array
+
+// var arr = new Array(1,2,3,4,5,"mostafa","hassounaaa");
+// console.log(arr);
+// console.log(arr[1]);
+// console.log(arr.length);
+
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     console.log(arr[i]);
+    
+// }
+
+
+
+// var arr1 = new Array(1,2,3,4,5,"mostafa");
+// var arr2 = new Array(6,7,8,9,0,"hasounaaa");
+
+// console.log(arr1);
+// console.log(arr1[4]);
+// console.log(arr2);
+// console.log(arr2[3]);
+
+// //insert array to arrayy
+// arr3 = arr1.concat(arr2);
+// console.log(arr3[11]);
+// console.log(arr1);
+
+// //insert value in the end of the array
+// arr1.push("test");
+// for (let i = 0; i < arr1.length; i++) {
+//     console.log(arr1[i]);
+    
+// }
+
+// DOM
+
+// document.getElementById('id1').innerHTML = "h1 have been edited";
+
+
+//click function and change style
+function clicked() {
+    // document.getElementById('id1').innerHTML = "h1 have been edited";
+    // document.getElementById('id1').style.color = "red";
+    // document.getElementById('id1').style.fontFamily = "Helvetica";
+    // document.write("<a href='jhjhjhjhj.com'>click</a>");
+    // console.log(document.cookie);
+}
